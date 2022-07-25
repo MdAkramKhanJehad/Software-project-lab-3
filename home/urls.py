99a28a1fc17991b83e79350deca57c7ee11677b8
MdAkramKhanJehad <bsse1007@iit.du.ac.bd>
2022-07-26 00:04:28 +0600

    routing done

--- home/urls.py
+++ home/urls.py
@@ -4,10 +4,10 @@ from home import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('home/search', views.search, name='search'),
+    path('search-routine', views.search, name='search'),
     path('tutorial', views.tutorial, name='tutorial'),
-    path('home/device_list', views.device_list, name='device_list'),
-    path('home/create_routine', views.create_routine, name='create_routine'),
-    path('home/confirmation', views.confirmation, name='confirmation'),
-    path('home/complete', views.complete, name='complete'),
+    path('device-list', views.device_list, name='device_list'),
+    path('create-routine', views.create_routine, name='create_routine'),
+    path('confirmation', views.confirmation, name='confirmation'),
+    path('complete', views.complete, name='complete'),
 ]
