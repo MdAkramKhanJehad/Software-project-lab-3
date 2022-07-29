35b1739f7f3f01f303bbb099add260aedbdd990e
MdAkramKhanJehad <bsse1007@iit.du.ac.bd>
2022-07-29 17:08:29 +0600

    user model bug fixed

--- home/views.py
+++ home/views.py
@@ -5,34 +5,45 @@ from login.models import NewUser
 
 def home(request):
     
-    if request.method == "POST":
+    all_users = NewUser.objects.all().filter(email=request.POST.get('email'))
+
+    if request.method == "POST" and len(all_users) < 1:
         email = request.POST.get('email')
         new_user = NewUser(email=email)
         new_user.save()
 
         print("Inside home inside if")
+    else:
+        print("USER available: ", all_users[0].id, " || ", all_users[0].email)
 
     print("Inside home out")
 
     return render(request, 'home/home.html')
 
+
 def search(request):
     return render(request, 'home/search/search.html')
+
 
 def tutorial(request):
     return render(request, 'home/tutorial/tutorial.html')
 
+
 def select_device(request):
     return render(request, 'home/create/select_device.html')
+
 
 def create_routine(request):
     return render(request, 'home/create/create_routine.html')
 
+
 def create_execution_indication(request):
     return render(request, 'home/create/create_execution_indicator.html')
 
+
 def confirmation(request):
     return render(request, 'home/confirmation/confirmation.html')
+
 
 def complete(request):
     return render(request, 'home/complete/complete.html')
