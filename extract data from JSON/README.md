4d77a00a335ee6904db2c0d5da358817b2970551
MdAkramKhanJehad <bsse1007@iit.du.ac.bd>
2022-08-05 09:53:06 +0600

    script ready for input file generation

---
+++ extract data from JSON/README.md
@@ -0,0 +1,7 @@
+# Get data as usable JSON
+
+## Step 1:
+ - Rename raw json file as "data.json"
+ - if there is any newly added device, which is in other category than others,  Write new device info in relevant categories list in extract_data_from_json.py file
+ - Output will be in "final.json" file
+ - Run this script and use the out final.json file as a input for device data source in main project
--- extract data from JSON/extract_data_from_json.py
+++ extract data from JSON/extract_data_from_json.py
@@ -1,34 +1,14 @@
 import json
 
 
-cat1 = ["Air Conditioner", "Air Cooler", "Beacon Device", "Clothes Dryer", "Coffee Maker", "Fan", "Humidifier", "Light Bulb", "Shades/Blinds", "Smart Printer", "Sprinkler", "Thermostat", "Vent", "Weather Station"]
-cat2 = ["Bread Maker", "Dishwasher", "Dust Detector", "Gas Stove", "Induction CookTop", "Kettle", "Microwave Oven", "Oil Warmer", "Refrigerator", "Rice Cooker", "Robot Vacuum", "Sous Vide", "SoyMilk Maker", "Water Heater"]
-cat3 = ["Audio Player", "Blood Pressure Monitor", "Door Lock", "Garage Door Opener", "Glass Break Detector", "Pulse Monitor", "Security Alarm", "Security Camera", "Sleep Monitor", "Smart Mirror", "Smart TV", "Speaker", "Water Leak Detector", "Water Purifier"]
-cat4 = ["CO Detector", "Gas Sensor", "Humidity Sensor", "Illuminance Sensor", "Moisture Sensor", "Motion Sensor", "Open/Close Sensor", "PH Sensor", "Presence Sensor", "Smart IR", "Smoke Detector", "Step Sensor", "Temperature Sensor", "Tilt Sensor"]
+appliances = ["Air Conditioner", "Air Cooler", "Beacon Device", "Clothes Dryer", "Coffee Maker", "Fan", "Humidifier", "Light Bulb", "Shades/Blinds", "Smart Printer", "Sprinkler", "Thermostat", "Vent", "Weather Station"]
+kitchenAndCleaning = ["Bread Maker", "Dishwasher", "Dust Detector", "Gas Stove", "Induction CookTop", "Kettle", "Microwave Oven", "Oil Warmer", "Refrigerator", "Rice Cooker", "Robot Vacuum", "Sous Vide", "SoyMilk Maker", "Water Heater"]
+safetyAndMultimedia = ["Audio Player", "Blood Pressure Monitor", "Door Lock", "Garage Door Opener", "Glass Break Detector", "Pulse Monitor", "Security Alarm", "Security Camera", "Sleep Monitor", "Smart Mirror", "Smart TV", "Speaker", "Water Leak Detector", "Water Purifier"]
+sensors = ["CO Detector", "Gas Sensor", "Humidity Sensor", "Illuminance Sensor", "Moisture Sensor", "Motion Sensor", "Open/Close Sensor", "PH Sensor", "Presence Sensor", "Smart IR", "Smoke Detector", "Step Sensor", "Temperature Sensor", "Tilt Sensor"]
 
 
 f = open('data.json')
 data = json.load(f)
-
-
-def get_device():
-    count = 0
-    count2 = 0
-    devices = []
-    for item in data:
-        for key in item.keys():
-            devices.append(key)
-            count += 1
-        count2 += 1
-
-
-    print(count)
-    print(count2)
-    print("device list length: ", len(devices))
-    return devices
-
-
-devices = get_device()
 
 
 dev_dict = {}
@@ -37,25 +17,27 @@ for item in data:
     device_name = list(item.keys())[0]
     item_keys = []
     desc = {}
+    attrAndDesc = []
     for i in item[device_name]:
         item_keys.append(list(i.keys())[0])
+        attrAndDesc.append(i)
 
-    if device_name in cat1:
+    if device_name in appliances:
         desc["Category"] = "Appliances"
         
-    elif device_name in cat2:
+    elif device_name in kitchenAndCleaning:
         desc["Category"] = "Kitchen & Cleaning"
 
-    elif device_name in cat3:
+    elif device_name in safetyAndMultimedia:
         desc["Category"] = "Safety & Multimedia"
     
-    elif device_name in cat4:
+    elif device_name in sensors:
         desc["Category"] = "Sensors"
     
     else:
         desc["Category"] = "Others"
     
-    desc["Attributes"] = item_keys
+    desc["AttributesAndDescriptions"] = attrAndDesc
     dev_dict[device_name]= desc
 
 json_object = json.dumps(dev_dict, indent = 4)
