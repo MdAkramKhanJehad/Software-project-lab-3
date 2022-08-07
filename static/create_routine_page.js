981124ee63f63f9a1a2aa22f4209d1f87944f3dc
MdAkramKhanJehad <bsse1007@iit.du.ac.bd>
2022-08-07 15:05:12 +0600

    scrolling  devices, add routine button completed

--- static/create_routine_page.js
+++ static/create_routine_page.js
@@ -1,5 +1,7 @@
 var currentTab = 0; // Current tab is set to be the first tab (0)
+var totalRoutine = 1;
 showTab(currentTab); // Display the current tab
+
 
 function showTab(n) {
     // This function will display the specified tab of the form...
@@ -71,3 +73,14 @@ function fixStepIndicator(n) {
     x[n].className += " active";
 }
 
+function addNewRoutine(){
+
+    // if (!validateForm()) return false;
+
+    const node = document.getElementById("routineField");
+    const clone = node.cloneNode(true);
+
+    document.getElementById("routineHolder").appendChild(clone); 
+    totalRoutine += 1;
+    console.log(totalRoutine);
+}
