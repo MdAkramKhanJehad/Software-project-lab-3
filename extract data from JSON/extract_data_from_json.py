1b2d7383e0de388bef7cc9e365caf4f8a6fc0eaa
MdAkramKhanJehad <bsse1007@iit.du.ac.bd>
2022-08-30 19:39:01 +0600

    added new script for getting from daniel data

--- extract data from JSON/extract_data_from_json.py
+++ extract data from JSON/extract_data_from_json.py
@@ -1,42 +1,64 @@
 import json
 
+appliances = ["Air Conditioner", "Refrigerator", "Air Cooler", "Clothes Dryer", "Fan", "Humidifier", "Thermostat", "Vent", "Dehumidifier", "Air Purifier", "Water Heater", "Electric Blanket" , "Airer"]
+securityAndSafety = ["CO Detector", "Smoke Detector", "Smart IR", "Door Lock", "Garage Door Opener", "Glass Break Detector", "Security Alarm", "Security Camera", "Water Leak Detector", "Doorbell", "Tilt Sensor", "Smart  Water Monitor and Shutoff Valve"]
+kitchenAndCleaning = ["Bread Maker", "Dishwasher", "Dust Detector", "Gas Stove", "Induction CookTop", "Kettle", "Microwave Oven", "Oil Warmer", "Rice Cooker", "Robot Vacuum", "Sous Vide", "SoyMilk Maker", "Coffee Maker", "Milk Dispenser", "Water Filter", "Water Purifier"]
+multimedia = ["Audio Player", "Smart Mirror", "Smart TV", "Speaker"]
+health = ["Blood Oximeter", "Blood Pressure Monitor", "Pulse Monitor", "Sleep Monitor"]
+lightsAndSwitches = ["Light Bulb", "Switches", "LED strip", "Scene Switch", "Lighting Remote", "Illuminance Sensor", "Button"]
+gardeningDevices = ["Sprinkler", "Valve", "Plant Grower", "Weather Station", "pH Sensor", "Moisture Sensor", "Temperature Sensor"]
+sensors = ["Gas Sensor", "Humidity Sensor", "Motion Sensor", "Open/Close Sensor", "Presence Sensor", "Step Sensor", "Door Sensor" ]
+others = ["Beacon Device", "Shades/Blinds", "Smart Printer", "Energy Meter", "Smart Battery", "Curtains", "Car Automation", "Geolocation Monitor", "Smart Assistant"]
 
-appliances = ["Air Conditioner", "Air Cooler", "Beacon Device", "Clothes Dryer", "Coffee Maker", "Fan", "Humidifier", "Light Bulb", "Shades/Blinds", "Smart Printer", "Sprinkler", "Thermostat", "Vent", "Weather Station"]
-kitchenAndCleaning = ["Bread Maker", "Dishwasher", "Dust Detector", "Gas Stove", "Induction CookTop", "Kettle", "Microwave Oven", "Oil Warmer", "Refrigerator", "Rice Cooker", "Robot Vacuum", "Sous Vide", "SoyMilk Maker", "Water Heater"]
-safetyAndMultimedia = ["Audio Player", "Blood Pressure Monitor", "Door Lock", "Garage Door Opener", "Glass Break Detector", "Pulse Monitor", "Security Alarm", "Security Camera", "Sleep Monitor", "Smart Mirror", "Smart TV", "Speaker", "Water Leak Detector", "Water Purifier"]
-sensors = ["CO Detector", "Gas Sensor", "Humidity Sensor", "Illuminance Sensor", "Moisture Sensor", "Motion Sensor", "Open/Close Sensor", "PH Sensor", "Presence Sensor", "Smart IR", "Smoke Detector", "Step Sensor", "Temperature Sensor", "Tilt Sensor"]
 
-
-f = open('data.json')
+f = open('daniel_data.json')
 data = json.load(f)
-
 
 dev_items = []
 
+
 for item in data:
     # print(item)
-    device_name = list(item.keys())[0]
+    device_name = item["name"]
     # print(device_name)
     item_keys = []
     desc = {}
     attrAndDesc = []
     device_details = []
-    for i in item[device_name]:
-        item_keys.append(list(i.keys())[0])
-        attrAndDesc.append(i)
+    for i in item["actions"]:
+        # item_keys.append(list(i.keys())[0])
+        temp_dict = {}
+        temp_dict[i["name"]] = i["command"]
+        temp_dict["desc"] = i["description"]
+        attrAndDesc.append(temp_dict)
+
 
     if device_name in appliances:
         desc["Category"] = "Appliances"
-        
+    
+    elif device_name in securityAndSafety:
+        desc["Category"] = "Security & Safety"
+    
     elif device_name in kitchenAndCleaning:
         desc["Category"] = "Kitchen & Cleaning"
 
-    elif device_name in safetyAndMultimedia:
-        desc["Category"] = "Safety & Multimedia"
+    elif device_name in multimedia:
+        desc["Category"] = "Multimedia"
+        
+    elif device_name in health:
+        desc["Category"] = "Health"
+        
+    elif device_name in lightsAndSwitches:
+        desc["Category"] = "Lights & Switches"
     
+    elif device_name in gardeningDevices:
+        desc["Category"] = "Gardening Devices"
+     
+    elif device_name in lightsAndSwitches:
+        desc["Category"] = "Lights & Switches"
+       
     elif device_name in sensors:
         desc["Category"] = "Sensors"
-    
     else:
         desc["Category"] = "Others"
     
@@ -52,24 +74,3 @@ print(json_object)
 
 with open("device_data.json", "w") as outfile:
     outfile.write(json_object)
-
-f2 = open('device_data.json')
-data2 = json.load(f2)
-
-# print(type(data2))
-
-for item in data2:
-    print(item)
-    device_name = list(item.keys())[0]
-    print("device: ", device_name)
-    for i in item[device_name]:
-        if list(i.keys())[0] == "Category":
-            print(list(i.keys())[0], " ", i[list(i.keys())[0]])
-
-        if list(i.keys())[0] == "AttributesAndDescriptions":
-            for attr in list(i.values())[0]:
-                print(list(attr.keys())[0], " : ", attr[list(attr.keys())[0]])
-                print(list(attr.keys())[1], " : ", attr[list(attr.keys())[1]])
-
-    print('\n')
-
