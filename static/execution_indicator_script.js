2c15c54e5d35f172d780569bb9a0b5f218900099
MdAkramKhanJehad <bsse1007@iit.du.ac.bd>
2022-09-09 23:42:32 +0600

    incomplete EI backend, till catching selected

--- static/execution_indicator_script.js
+++ static/execution_indicator_script.js
@@ -16,4 +16,20 @@ function getPreviouslyCreatedRoutinesFromSession(){
     for (let i = 0; i < previouslyCreatedRoutines.length; i++) {
         console.log("Routines: " + " " + previouslyCreatedRoutines[i][0] + " -> " + previouslyCreatedRoutines[i][1]);
     }
+
+    
 }
+
+for (let i = 1; i < previouslyCreatedRoutines.length + 1; i++) {
+    for (let j = 1; j < 6; j++) {
+        var eiId = "#ei-" + j + "-routine-" + i;
+        $(eiId).on('click',function() {
+            if($(this).val() != "Select"){
+                console.log("Routine:" + i + " EI:" + j + $(this).val());
+            }
+            
+        });
+    }
+}
+
+
