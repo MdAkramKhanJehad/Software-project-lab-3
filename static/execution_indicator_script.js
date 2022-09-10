d3f90ccabd070ca2c922a56474a32ab6ba2be352
MdAkramKhanJehad <bsse1007@iit.du.ac.bd>
2022-09-10 11:19:58 +0600

    EI frontend with field validation done

--- static/execution_indicator_script.js
+++ static/execution_indicator_script.js
@@ -1,5 +1,12 @@
 var previouslyCreatedRoutines;
+var executionIndicatorsList = [];
+var totalDefinedEiCount = 0;
+
 getPreviouslyCreatedRoutinesFromSession();
+
+if(totalDefinedEiCount !=  previouslyCreatedRoutines.length * 5){
+    document.getElementById('nextBtn').className += " disabled";
+}
 
 
 function getPreviouslyCreatedRoutinesFromSession(){
@@ -13,10 +20,12 @@ function getPreviouslyCreatedRoutinesFromSession(){
 
     previouslyCreatedRoutines = JSON.parse(previouslyCreatedRoutines);
 
-    for (let i = 0; i < previouslyCreatedRoutines.length; i++) {
-        console.log("Routines: " + " " + previouslyCreatedRoutines[i][0] + " -> " + previouslyCreatedRoutines[i][1]);
-    }
+    // for (let i = 0; i < previouslyCreatedRoutines.length; i++) {
+    //     console.log("Routines: " + " " + previouslyCreatedRoutines[i][0] + " -> " + previouslyCreatedRoutines[i][1]);
+    // }
 
+    for(let i=0; i<previouslyCreatedRoutines.length; i++)
+        executionIndicatorsList.push([]);
     
 }
 
@@ -24,12 +33,25 @@ for (let i = 1; i < previouslyCreatedRoutines.length + 1; i++) {
     for (let j = 1; j < 6; j++) {
         var eiId = "#ei-" + j + "-routine-" + i;
         $(eiId).on('click',function() {
-            if($(this).val() != "Select"){
-                console.log("Routine:" + i + " EI:" + j + $(this).val());
+            if($(this).val() != null){
+
+                if(executionIndicatorsList[i-1][j-1] == undefined){
+                    totalDefinedEiCount += 1;
+                    
+                    if(totalDefinedEiCount ==  previouslyCreatedRoutines.length * 5){
+                        document.getElementById('nextBtn').className = document.getElementById('nextBtn').className.replace(" disabled", ""); 
+                    }
+                }
+                
+                executionIndicatorsList[i-1][j-1] = $(this).val();
+
+                console.log("Total:" + totalDefinedEiCount + " | Routine:" + i + " | EI:" + j + $(this).val());
             }
             
         });
     }
 }
+
+
 
 
