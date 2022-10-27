743afd0329fd4fb395c05ba12c749b54c52596a1
MdAkramKhanJehad <bsse1007@iit.du.ac.bd>
2022-10-27 20:43:20 +0600

    incomplete download routine

--- static/search_page_script.js
+++ static/search_page_script.js
@@ -1,8 +1,23 @@
-
 var currentSearchType = $("#btn-group input[type='radio']:checked").val();
 document.getElementById("search-type").value = currentSearchType;
 console.log("Hello from search" + currentSearchType);
 console.log("Hell search 2: " + document.getElementById("search-type").value);
+
+
+function getSearchResult(){
+    var searchResult = document.getElementById("created-routines").getAttribute("data-search-results");
+   
+    searchResult = searchResult.replaceAll("['", '["');
+    searchResult = searchResult.replaceAll("',", '",');
+    searchResult = searchResult.replaceAll(" '", ' "');
+    searchResult = searchResult.replaceAll("']", '"]');
+    searchResult = searchResult.replaceAll("'", "\'");
+
+    searchResult = JSON.parse(searchResult);
+
+    console.log("Length:" + searchResult.length);
+    return searchResult;
+}
 
 
 $('#btn-group').on('change', function() {
@@ -10,6 +25,19 @@ $('#btn-group').on('change', function() {
     document.getElementById("search-type").value = currentSearchType;
     console.log("CurSel:" + currentSearchType + " | " + document.getElementById("search-type").value);
 });
+
+
+// function downloadObjectAsJson(){
+//     var searchResult = getSearchResult();
+
+//     var dataStr = "data:text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(searchResult));
+//     var downloadAnchorNode = document.createElement('a');
+//     downloadAnchorNode.setAttribute("href", dataStr);
+//     downloadAnchorNode.setAttribute("download", "routines.json");
+//     document.body.appendChild(downloadAnchorNode); // required for firefox
+//     downloadAnchorNode.click();
+//     downloadAnchorNode.remove();
+// }
 
 
 // function searchRoutine(){
